62d4883eb21367a5f2aaf42f0cc973ab2c26656a
jbc5099 <jbc5099@gmail.com>
2019-01-13 22:31:26 -0700

    user-interaction

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ let first = document.getElementById("first");
 console.log(first);
 first.textContent = "Bounjure";
 let third = document.getElementById("third");
-for(let i = 0; i < 10; i++){
+function count(){
     const punctuation = document.createElement("span");
     punctuation.textContent = "!";
-    third.parentNode.insertBefore(punctuation, third.nextSibling);
+    third.parentNode.insertBefore(punctuation, third.nextSibling);    
 }
+first.onclick = count;
