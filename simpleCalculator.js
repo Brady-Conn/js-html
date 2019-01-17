99a233d73bbd9f1770bd49369290f8a867ed53cc
jbc5099 <jbc5099@gmail.com>
2019-01-16 23:02:42 -0700

    updated approach, input methods stable

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -18,94 +18,68 @@ let display = document.getElementById("display");
 let operation = document.getElementById("operation")
 let answer = document.getElementById("answer")
 let input = "";
+let parse = [];
+let operators = ['+', '-', '*', '/'];
 
 function addToDisplay(e){
-    let selection = e.target;
-    operation.textContent += selection.textContent;
+    let selection = e.target.textContent;
+    if(operation.textContent === '' && operators.includes(selection)){
+        operation.textContent = answer.textContent + selection;
+        parse.push(answer.textContent);
+        parse.push(selection);
+        console.log("1")
+    }
+    else if(operators.includes(parse[parse.length-1]) === true && operators.includes(parse[parse.length-2]) === false && selection === '-'){
+        operation.textContent += selection;
+        parse.push(selection);
+        console.log("2")
+    }
+    else if(operators.includes(parse[parse.length -1]) && operators.includes(selection)){
+        return;
+    }
+    else if((operators.includes(parse[parse.length-1]) && parse[parse.length-1] !== '-') || operation.textContent === ''){
+        operation.textContent += selection;
+        parse.push(selection);
+        console.log('3')
+    }
+    else if(operators.includes(selection) === false || parse[parse-1] === '-'){
+        operation.textContent += selection;
+        parse[parse.length-1] += selection;
+        console.log('4')
+    }
+    else{
+        operation.textContent += selection;
+        parse.push(selection);
+        console.log('5')
+    }
+    console.log(parse)
 }
 
 function clearAll(e){
     answer.textContent = "0";
     operation.textContent = "";
+    parse = [];
 }
 
 
-    one.onclick = addToDisplay;
-    two.onclick = addToDisplay;
-    three.onclick = addToDisplay;
-    four.onclick = addToDisplay;
-    five.onclick = addToDisplay;
-    six.onclick = addToDisplay;
-    seven.onclick = addToDisplay;
-    eight.onclick = addToDisplay;
-    nine.onclick = addToDisplay;
-    zero.onclick = addToDisplay;
-    add.onclick = addToDisplay;
-    subtract.onclick = addToDisplay;
-    multiply.onclick = addToDisplay;
-    divide.onclick = addToDisplay;
-
+one.onclick = addToDisplay;
+two.onclick = addToDisplay;
+three.onclick = addToDisplay;
+four.onclick = addToDisplay;
+five.onclick = addToDisplay;
+six.onclick = addToDisplay;
+seven.onclick = addToDisplay;
+eight.onclick = addToDisplay;
+nine.onclick = addToDisplay;
+zero.onclick = addToDisplay;
+add.onclick = addToDisplay;
+subtract.onclick = addToDisplay;
+multiply.onclick = addToDisplay;
+divide.onclick = addToDisplay;
 
 function calculate(e){
-    let parse = [];
-    input = operation.textContent;
-    let syntaxCheck = /\*(?=\d)|\/(?=\d)|\+(?=\d)|\-(?=\d)|\-{2}/;
-    let lastCharCheck = /\*$|\+$|\/$|\-$/;
-    let firstCharCheck = /^\*|^\+|^\/|^\-/;
-    let numberCheck = /\d/;
-    let total = 0;
-    let operatorCheck = ["+", "-", "/", "*"];
-    if(syntaxCheck.test(input) === 0 || input.match(lastCharCheck) !== null){
-        answer.textContent = "Invalid syntax";
-        return;
-    }
 
-
-    for(let i = 0; i < input.length; i++){
-        if(input[i].match(numberCheck) !== null && (i === 0 || operatorCheck.includes(input[i-1]))){
-          parse.push(input[i]);
-        }
-        else if((input[i].match(numberCheck) !== null || input[i] === ".") && parse.length === 1){
-            parse[0] += input[i];
-        }
-        else if(input[i].match(numberCheck) !== null && parse.length === 3){
-            parse[2] += input[i];
-        }
-        else if(input[i].match(numberCheck) !== null && parse.length === 4){
-            parse[3] += input[i];
-        }
-        else{ 
-            parse.push(input[i])
-        }
-    }
-
-    if(parse.length === 4){
-        parse[2] = parse[3];
-        parse.pop;
-        parse[1] = "+";
-    }
-    
-    if(parse[1] === "+"){
-        total = parseInt(parse[0]) + parseInt(parse[2]);
-    }
-    else if(parse[1] === "*"){
-        total = parseInt(parse[0]) * parseInt(parse[2]);
-    }
-    else if(parse[1] === "/"){
-        total = parseInt(parse[0]) / parseInt(parse[2])
-    }
-    else if(parse[1] === "-"){
-        total = parseInt(parse[0]) - parseInt(parse[2])
-    }
-    console.log(total)
-    answer.textContent = total;
-    console.log(answer.textContent)
-    if(answer.textContent === "0"){
-        operation.textContent = "";
-    }
-    else{operation.textContent = total;
-    }
 }
 
-equals.onclick = calculate;
+
 clear.onclick = clearAll;
